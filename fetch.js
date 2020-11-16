f1c6d5cdb54925605f6c0a1cb968f64ab8b7e88a
Niredocz <daffa.rogers@gmail.com>
2020-11-16 08:43:35 +0700

    set XML Http request

---
+++ fetch.js
@@ -0,0 +1,16 @@
+const getBtn = document.getElementById('get');
+const postBtn = document.getElementById('post');
+
+
+// https://reqres.in/api/users?page=2
+const getData = () => {
+    fetch('https://reqres.in/api/users?page=2');
+}
+
+// https://reqres.in/api/register
+const postData = () => {
+    
+}
+
+getBtn.addEventListener('click', getData);
+postBtn.addEventListener('click', postData);
