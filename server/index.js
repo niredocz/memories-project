49591f02106afaa5be71e31b358858be1e63a102
Niredocz <daffa.rogers@gmail.com>
2020-11-16 15:23:31 +0700

    initial commit

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 
-const CONNECTION_URL = 'mongodb+srv://niredocz:niredocz123@cluster0.vijgn.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://niredocz:niredocz123@cluster0.vijgn.mongodb.net/db_memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
