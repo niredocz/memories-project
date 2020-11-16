4ff05f807e382cf3dcae0c001bb6b6023e07ebd1
Niredocz <daffa.rogers@gmail.com>
2020-11-16 14:43:50 +0700

    update server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
-import axios from'axios';
+import axios from 'axios';
 
 const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
-export const createPosts = (newPosts) => axios.post(url, newPosts)
+export const createPosts = (newPost) => axios.post(url, newPost);
