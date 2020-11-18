34b71b3ebb9992ec590163ffc8f0de5e3453d1d6
Niredocz <daffa.rogers@gmail.com>
2020-11-18 13:15:37 +0700

    update filebase64 package

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+// import { useDispatch } from 'react-redux';
+
 import useStyles from './style';
 
-function Post( { post }) {
+function Post({ post }) {
+    // const dispatch = useDispatch();
     const classes = useStyles();
     return(
         <Card className={classes.card}>
-            <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
+            <CardMedia style={{height: 0, paddingTop: '56.25%'}} className={classes.media} image={post.selectedFile} title={post.title} />
             <div className={classes.overlay}>
                 <Typography variant="h6">{post.creator}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
