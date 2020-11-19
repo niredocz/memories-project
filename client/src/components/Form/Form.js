2b3070699ae816197307be8a15258b51b0717bd1
Niredocz <daffa.rogers@gmail.com>
2020-11-19 09:37:36 +0700

    update file-base64 server

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-import FileBase64 from './react-file-base64.js';
+import FileBase64 from 'react-file-base64';
 
 import useStyles from './styles';
 import { createPost } from '../../actions/posts'
@@ -19,7 +19,8 @@ function Form() {
         dispatch(createPost(postData))
     }
     const clear = () => {
-
+        // setCurrentId(0);
+        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
     }
     return(
         <Paper className={classes.paper}>
@@ -29,7 +30,7 @@ function Form() {
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
                 <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
-                <div className={classes.fileInput}><FileBase64 type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})} /></div>
+                <div className={classes.fileInput}><FileBase64 type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
             </form>
