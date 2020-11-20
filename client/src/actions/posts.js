9f38ef9f4be822620b68b35c01779c38c36c528a
Niredocz <daffa.rogers@gmail.com>
2020-11-20 09:24:26 +0700

    add scalable const name

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,23 +1,21 @@
+import { UPDATE, CREATE, DELETE, FETCH_ALL } from '../constants/constantsTypes.js';
 import * as api from '../api';
 
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
 
-        dispatch({ type: 'FETCH_ALL', payload: data })
-
+        dispatch({ type: FETCH_ALL, payload: data })
     } catch (error) {
         console.error(error.message);
     }
-    
-    
 }
 
 export const createPost = (post) => async (dispatch) => {
     try {
         const {data} = await api.createPost(post)
 
-        dispatch({ type: 'CREATE', payload: data})
+        dispatch({ type: CREATE, payload: data})
     } catch (error) {
         console.log(error);
     }
@@ -27,7 +25,7 @@ export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id, post);
 
-        dispatch({ type: 'UPDATE', payload: data })
+        dispatch({ type: UPDATE, payload: data })
     } catch (error) {
         console.log(error)
     }
@@ -37,7 +35,7 @@ export const deletePost = (id) => async (dispatch) => {
     try {
         await api.deletePost(id);
 
-        dispatch({ type: 'DELETE', payload: id });
+        dispatch({ type: DELETE, payload: id });
     } catch (error) {
         console.log(error);
     }
@@ -47,7 +45,7 @@ export const likePost = (id) => async (dispatch) => {
     try {
         const { data } = await api.likePost( id );
 
-        dispatch({ type: 'UPDATE', payload: data });
+        dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         console.log(error);
     }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,14 +1,14 @@
+import { UPDATE, CREATE, DELETE, FETCH_ALL } from '../constants/constantsTypes.js';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'UPDATE':
-        case 'LIKE':
+        case UPDATE:
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload);
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
         default:
             return posts;
