d269ba657c2207a399b239aff89ab71d8df4d2b1
niredocz <daffaaldzakian20@gmail.com>
2020-12-28 10:51:34 +0700

    initial commit

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -57,6 +57,6 @@ export const likePost = async ( req, res ) => {
 
     const post = await PostMessage.findById(id);
     const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
-        
+
     res.json(updatedPost);
 }
